301497a533494daba268f5eaacb241bd5f498d25
Sander Kolman <sanderkolman@gmail.com>
2019-05-01 13:39:37 +0200

    added callbackToPromise function SAH-905

--- index.js
+++ index.js
@@ -393,6 +393,8 @@ module.exports = {
     return functionName;
   },
 
+  CtP: callbackToPromise,
+
   AWS: AWS,
 
   AWSXRay: AWSXRay
@@ -552,4 +554,14 @@ function hideVulnerableKeys(key, val) {
   if(additionalSecretFilter && additionalSecretFilter.test(key))
     return "***"
   return val
+}
+
+function callbackToPromise(fn) {
+  return function(...args) {
+    return new Promise((res, rej) => {
+      let callbackHandle = (error, result) => { error? rej(error): res(result) }
+      args.push(callbackHandle)
+      fn.apply(this, args)
+    })
+  }
 }
